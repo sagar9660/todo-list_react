c3d99c42cfbeb00723959d437f0204c173fca5ae
Sagar Singh <sagar966028@gmail.com>
2022-07-26 12:15:55 +0530

    Inital commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+      This is an about component
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam eveniet repellat magni saepe minus culpa quisquam delectus natus quae. Corporis quibusdam atque eaque expedita doloribus at, consectetur consequuntur dolor sequi praesentium illo, natus error.</p>
+    </div>
+  )
+}
